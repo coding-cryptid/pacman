6dcb7a73f00a55ab414d0c8a3e2a92100f411776
giannag27 <giacosey28@gmail.com>
2026-02-17 11:47:13 -0500

    Fixed image load from folder. (tutorial did not put images in an image folder but I did)

--- pacman.js
+++ pacman.js
@@ -18,11 +18,14 @@ let pacmanLeftImage;
 let pacmanRightImage;
 let wallImage;
 
+//load functions
 window.onload = function() {
     gameBoard = this.document.getElementById("gameBoard");
     gameBoard.height = gameBoardHeight;
     gameBoard.width = gameBoardWidth;
     context = gameBoard.getContext("2d");
+
+    loadImages();
 }
 
 function loadImages() {
@@ -30,26 +33,26 @@ function loadImages() {
     wallImage.src = "./wall.png";
 
     blueGhostImage = new Image();
-    blueGhostImage.src = "./blueGhost.png";
+    blueGhostImage.src = "./images/blueGhost.png";
 
     redGhostImage = new Image();
     redGhostImage.src = "./redGhost.png";
 
     pinkGhostImage = new Image();
-    pinkGhostImage.src = "./pinkGhost.png";
+    pinkGhostImage.src = "./images/pinkGhost.png";
 
     orangeGhostImage = new Image();
     orangeGhostImage.src = "./orangeGhost.png";
 
     pacmanUpImage = new Image();
-    pacmanUpImage.src = "./pacmanUp.png";
+    pacmanUpImage.src = "./images/pacmanUp.png";
 
     pacmanDownImage = new Image();
     pacmanDownImage.src = "./pacmanDown.png";
 
     pacmanLeftImage = new Image();
-    pacmanLeftImage.src = "./pacmanLeft.png";
+    pacmanLeftImage.src = "./imagespacmanLeft.png";
 
     pacmanRightImage = new Image();
-    pacmanRightImage.src = "./pacmanRight.png";
+    pacmanRightImage.src = "./images/pacmanRight.png";
 }
